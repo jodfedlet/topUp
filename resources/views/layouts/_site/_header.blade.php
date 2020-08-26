0f39e00044fa412994789713aac1ce2fe337be68
makender103 <makender103@gmail.com>
2020-08-26 16:13:03 -0300

    set the footer

--- resources/views/layouts/_site/_header.blade.php
+++ resources/views/layouts/_site/_header.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="fr">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
       <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Nuvann - @yield('title')</title>
+    <title>Top up - @yield('title')</title>
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
